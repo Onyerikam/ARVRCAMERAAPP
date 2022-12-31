d05c4a6daf381b4db8e91d199a8e9b2e85ca80f6
Onyerikam <115038141+Onyerikam@users.noreply.github.com>
2022-12-31 21:05:36 +0000

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,144 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import ARKit from 'react-native-arkit';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import * as MLKit from 'react-native-ml-kit';
 
-export default function App() {
+const ARCameraApp = () => {
+  const [isAREnabled, setIsAREnabled] = useState(false);
+  const [isVREnabled, setIsVREnabled] = useState(false);
+  const [isObjectRecognitionEnabled, setIsObjectRecognitionEnabled] = useState(false);
+  const [objectRecognitionResult, setObjectRecognitionResult] = useState(null);
+  const [isFilterEnabled, setIsFilterEnabled] = useState(false);
+  const [isNavigationEnabled, setIsNavigationEnabled] = useState(false);
+  const [destination, setDestination] = useState(null);
+  const [currentLocation, setCurrentLocation] = useState(null);
+
+  const toggleAR = () => {
+    setIsAREnabled(!isAREnabled);
+  };
+
+  const toggleVR = () => {
+    setIsVREnabled(!isVREnabled);
+  };
+
+  const toggleObjectRecognition = () => {
+    setIsObjectRecognitionEnabled(!isObjectRecognitionEnabled);
+  };
+
+  const toggleFilter = () => {
+    setIsFilterEnabled(!isFilterEnabled);
+  };
+
+  const toggleNavigation = () => {
+    setIsNavigationEnabled(!isNavigationEnabled);
+  };
+
+  const pickImage = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      if (!result.cancelled) {
+        const visionResponse = await MLKit.ObjectDetection.detectObjectsOnImage(result.uri);
+        setObjectRecognitionResult(visionResponse);
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={{ flex: 1 }}>
+      {isAREnabled ? (
+        <ARKit
+          style={{ flex: 1 }}
+          debug
+          planeDetection
+          lightEstimation
+          onARKitError={console.warn}
+        >
+          {objectRecognitionResult && (
+            <ARKit.Text
+              text={objectRecognitionResult.map(obj => obj.label).join(', ')}
+              position={{ x: 0, y: 0, z: -0.5 }}
+            />
+          )}
+          {isNavigationEnabled && currentLocation && (
+            <ARKit.Model
+              scale={0.01}
+              position={currentLocation}
+              model={{
+                file: 'arrow.scnassets/arrow.scn',
+              }}
+            />
+          )}
+          {isNavigation enabled && destination && (
+            <ARKit.Model
+            scale={0.01}
+            position={destination}
+            model={{
+              file: 'flag.scnassets/flag.scn',
+            }}
+          />
+        )}
+      </ARKit>
+    ) : isVREnabled ? (
+      <VRView style={{ flex: 1 }}>
+        {/* Add your VR content here */}
+      </VRView>
+    ) : (
+      <RNCamera
+        style={{ flex: 1 }}
+        type={RNCamera.Constants.Type.front}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+      >
+        {isFilterEnabled && (
+          <Image
+            style={{ flex: 1 }}
+            source={require('./filter.png')}
+            resizeMode="cover"
+          />
+        )}
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text onPress={toggleAR}>Toggle AR</Text>
+          <Text onPress={toggleVR}>Toggle VR</Text>
+          {isObjectRecognitionEnabled ? (
+            <Text onPress={toggleObjectRecognition}>Disable Object Recognition</Text>
+          ) : (
+            <Text onPress={toggleObjectRecognition}>Enable Object Recognition</Text>
+          )}
+          {isFilterEnabled ? (
+            <Text onPress={toggleFilter}>Disable Filter</Text>
+          ) : (
+            <Text onPress={toggleFilter}>Enable Filter</Text>
+          )}
+          {isNavigationEnabled ? (
+            <Text onPress={toggleNavigation}>Disable Navigation</Text>
+          ) : (
+            <Text onPress={toggleNavigation}>Enable Navigation</Text>
+          )}
+          <Text onPress={pickImage}>Pick image for object recognition</Text>
+        </View>
+      </RNCamera>
+    )}
+  </View>
+);
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default ARCameraApp;
